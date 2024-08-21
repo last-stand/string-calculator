c8901ba1eaf483e9d5f580feaccda7a3d7f1b4ad
🍀 Jai <jplko.simt@gmail.com>
2024-08-21 18:10:16 +0530

    Refactoring add method in StringCalculator

--- src/main/java/org/example/StringCalculator.java
+++ src/main/java/org/example/StringCalculator.java
@@ -11,30 +11,52 @@ public class StringCalculator {
     private static final int MAX_ALLOWED_INTEGER = 1000;
 
     public static int add(String string) {
-        String delimiter = ",\n";
-        if (!string.isEmpty()) {
-            String lastTwoChars = string.length() > 1 ? string.substring(string.length() - 2) : "";
-            if (lastTwoChars.equals(",\n")) {
-                throw new InvalidInputException();
-            }
-            String fistThreeChars = string.length() > 2 ? string.substring(0, 2) : "";
-            if (fistThreeChars.equals("//")) {
-                String[] stringArray = string.split("\n");
-                String delimiterInput = stringArray[0];
-                string = stringArray[1];
-                delimiter = String.valueOf(delimiterInput.charAt(2));
-            }
-            List<Integer> integerList = Arrays.stream(string.split("[" + delimiter + "]"))
-                    .mapToInt(Integer::parseInt).boxed().toList();
-            String negativeNumbersString = integerList.stream()
-                    .filter((num) -> num < 0)
-                    .map(String::valueOf)
-                    .collect(Collectors.joining(","));
-            if (!negativeNumbersString.isEmpty()) {
-                throw new NegativesNotAllowedException(negativeNumbersString);
-            }
-            return integerList.stream().mapToInt(Integer::intValue).filter(num -> num <= MAX_ALLOWED_INTEGER).sum();
+        if (string.isEmpty()) {
+            return 0;
         }
-        return 0;
+        String delimiter = ",\n";
+        if (string.endsWith(",\n")) {
+            throw new InvalidInputException();
+        }
+        if (string.startsWith("//")) {
+            String[] stringParts = splitInput(string);
+            delimiter = extractDelimiter(stringParts[0]);
+            string = stringParts[1];
+        }
+        List<Integer> integerList = parseNumbers(string, "[" + delimiter + "]");
+        checkNegativeNumbers(integerList);
+        return sumValidNumbers(integerList);
+    }
+
+    private static int sumValidNumbers(List<Integer> integerList) {
+        return integerList.stream().mapToInt(Integer::intValue).filter(num -> num <= MAX_ALLOWED_INTEGER).sum();
+    }
+
+    private static String[] splitInput(String input) {
+        String[] stringParts = input.split("\n", 2);
+        if (stringParts.length != 2 || stringParts[1].isEmpty()) {
+            throw new InvalidInputException();
+        }
+        return stringParts;
+    }
+
+    private static String extractDelimiter(String delimiterInput) {
+        return String.valueOf(delimiterInput.charAt(2));
+    }
+
+    private static void checkNegativeNumbers(List<Integer> numbers) {
+        String negativeNumbersString = numbers.stream()
+                .filter((num) -> num < 0)
+                .map(String::valueOf)
+                .collect(Collectors.joining(","));
+        if (!negativeNumbersString.isEmpty()) {
+            throw new NegativesNotAllowedException(negativeNumbersString);
+        }
+    }
+
+    private static List<Integer> parseNumbers(String input, String delimiter) {
+        return Arrays.stream(input.split(delimiter))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
     }
 }
